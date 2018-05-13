1fc2ed1746b6fed72fac3eee522e4141fdb6e5d6
Stefan Hoffmann <sthoffmann@onlinehome.de>
2018-05-13 10:15:05 +0200

    init

---
+++ question-variables-and-quotes.ps1
@@ -0,0 +1,18 @@
+<#
+
+    Question: 
+    You want to supply text as input to a parameter. You want to ensure that no variables named in the text are automatically replaced with their values. Which symbols should you use to enclose the text?
+
+    Answer: 
+    Double Quotes
+
+    Correct Answer should be: 
+    Single Quotes
+
+    Demo:
+#>
+$variable = "World"
+
+Write-Host "Hello $variable"
+Write-Host 'Hello $variable'
+
